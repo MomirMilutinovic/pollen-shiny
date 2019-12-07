0a203e7430f6b24cc02bc0c62f63c65c213ab5d3
Momir Milutinovic <milutinm462@gmail.com>
2019-12-07 23:37:38 +0100

    First working shiny app

---
+++ PolenNaDan/ReadData.R
@@ -0,0 +1,38 @@
+#Reading the list of stations
+stationList <- read_json("http://polen.sepa.gov.rs/api/opendata/locations/")
+locations <- data.frame(matrix(unlist(stationList), nrow=length(stationList), byrow=T))
+names(locations) <- c("ID", "Naziv", "lat", "long", "podaci")
+
+#Reading the list of concetrations
+pollenList <- read_json("http://polen.sepa.gov.rs/api/opendata/pollens/")
+
+#pollenList[4][[1]] contains the actual data
+pollenList <- pollenList[4][[1]]
+
+pollen <- data.frame(id = integer(), long = numeric(), lat = numeric(), date = as.Date(character()),
+                     concetration = numeric())
+
+#Parsing json by iterating through the lists returned from read_json
+#and putting entries into a data.frame
+for(i in 1:length(pollenList)){
+  id <- pollenList[[i]]$id
+  locationID <- pollenList[[i]]$location
+  date <- as.Date(pollenList[[i]]$date)
+  concetrations <- pollenList[[i]]$concentrations
+  
+  index <- which(locations$ID == locationID)
+  long <- as.numeric(as.character(locations$long[index]))
+  lat <- as.numeric(as.character(locations$lat[index]))
+  
+  if(length(concetrations) == 0){
+    next
+  }
+  
+  if(lat == 0 & long == 0){
+    next
+  }
+  
+  for(j in 1:length(concetrations)){
+    pollen[nrow(pollen) + 1,] = list(i, long, lat, date, concetrations[[j]])
+  }
+}
---
+++ PolenNaDan/app.R
@@ -0,0 +1,78 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library("shiny")
+library("jsonlite")
+library("leaflet")
+library("dplyr")
+library("tidyr")
+library("RColorBrewer")
+
+source("ReadData.R")
+
+# Define UI for application that draws a map
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Pollen on a given day"),
+
+    sidebarLayout(
+        sidebarPanel(
+            dateInput("date",
+                        "Date:"),
+        ),
+
+        # Show a map
+        mainPanel(
+            leafletOutput("map", width = "100%", height = 600)
+        )
+    )
+)
+
+# Define server logic required to draw a map
+server <- function(input, output) {
+    
+    mapData <- reactive({
+        md <- pollen
+        indices <- which(md$date == input$date)
+        md <- md[indices,]
+    })
+
+    output$map <- renderLeaflet({
+        md <- mapData()
+        
+        if(nrow(md) == 0){
+            #Draw an empty map if there is no data for
+            #the given day
+            leaflet() %>%
+                addTiles() %>%
+                fitBounds(18, 41, 23, 46)
+        }else{
+            #Color coding values
+            colors <- vector()
+            categories <- cut(md$concetration, breaks = 8)
+            
+            #Asigning colors
+            for(i in 1:length(md$concetration))
+                colors <- c(colors, brewer.pal(n = 8, name = 'Greens')[as.integer(categories[i])])
+            
+            #Drawing map
+            leaflet(data = md) %>%
+                addTiles() %>%
+                fitBounds(min(md$long), min(md$lat), max(md$long), max(md$lat)) %>%
+                addCircleMarkers(lng = ~long, lat = ~lat,
+                                 radius = 10, weight = 5, color = "black",
+                                 fillColor = ~colors, fillOpacity = 0.7, popup = ~as.character(concetration))
+        }
+    })
+        
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
